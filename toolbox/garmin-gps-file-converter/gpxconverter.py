33c6cf33177123781f232043c0d10afbfa33c8b8
Michael Akridge <michael.akridge@noaa.gov>
2024-07-23 16:58:50 -1000

    update

--- toolbox/garmin-gps-file-converter/gpxconverter.py
+++ toolbox/garmin-gps-file-converter/gpxconverter.py
@@ -1,12 +1,3 @@
-# Compilation mode, standalone everywhere, except on macOS there app bundle
-# nuitka-project-if: {OS} in ("Windows", "Linux", "FreeBSD"):
-#    nuitka-project: --onefile
-# nuitka-project-if: {OS} == "Darwin":
-#    nuitka-project: --standalone
-#    nuitka-project: --macos-create-app-bundle
-#    nuitka-project: --macos-disable-console
-#
-
 import re
 import numpy as np
 from datetime import datetime, timedelta
